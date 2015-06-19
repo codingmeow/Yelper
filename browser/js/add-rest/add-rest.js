5028fc598865caf7f9017f413a4532adef222ced
Jennifer Cui <jcui@Jennifers-MacBook.local>
2015-06-19 19:10:34 -0400

    can't figure out. i can't angular

--- browser/js/add-rest/add-rest.js
+++ browser/js/add-rest/add-rest.js
@@ -1,3 +1,17 @@
-app.config(function ($stateProvider) {
-	
-} )
+// app.config(function ($stateProvider) {
+// 	$stateProvider.state('addRestaurant', {
+// 		url: "/newRest",
+// 		controller: "NewRestController",
+// 		templateUrl: "js/add-rest/add-rest.html"
+// 	});
+// });
+
+// app.controller('NewRestController', function ($scope, RestFactory){
+// 	$scope.inputRest = function (rest){
+// 		RestFactory.addRest(rest).then(function (newRest){
+// 			$scope.newRest = {
+// 				url: null
+// 			}
+// 		})
+// 	}
+// })
