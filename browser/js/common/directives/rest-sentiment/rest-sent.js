03febe96461b3f24143d97442b1d2b4c869fc173
Jennifer Cui <jcui@Jennifers-MacBook.local>
2015-06-22 11:11:15 -0400

    resolve conflict

---
+++ browser/js/common/directives/rest-sentiment/rest-sent.js
@@ -0,0 +1,12 @@
+// app.directive('restSentiment', function ($state){
+// 	return {
+// 		restrict: 'E',
+// 		scope: {
+// 			restaurant: '='
+// 		},
+// 		templateUrl: 'js/common/directives/rest-sentiment/rest-sent.html'//,
+// 		// link: function (scope) {
+			
+// 		// }
+// 	};
+// });
